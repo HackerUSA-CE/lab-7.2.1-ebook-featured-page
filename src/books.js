b972713cb259f0d9e2e7dddf576c12f76b48ebbf
Joshua Miles <joshua.m@hackerusa.com>
2020-09-03 09:09:14 -0500

    master

---
+++ src/books.js
@@ -0,0 +1,26 @@
+export const books = [
+    {
+        title: 'The Shinobi Initialive',
+        description: 'The reality-bending adventures of a clandestine service agency in the year 2166',
+        year: 2014,
+        imageURL: '/assets/shinobi-initiative.jpeg'
+    },  
+    {
+        title: 'Tess the Wonder Dog',
+        description: 'The tale of a dog who gets super powers',
+        year: 2007,
+        imageURL: '/assets/tess.jpg'
+    },
+    {
+        title: 'The Annals of Arathrae', 
+        description: 'This anthology tells the intertwined narratives of six fairy tales.',
+        year: 2016,
+        imageURL: '/assets/arathrae.jpeg'
+    }, 
+    {
+        title: 'W∀RP',
+        description: 'A time-space anomaly folds matter from different points in earth\'s history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.',
+        year: 2010,
+        imageURL: '/assets/warp.jpeg'
+    }
+]
